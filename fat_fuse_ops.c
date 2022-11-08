216a0b88dc857cd1d7adededc078b2af88e7b579
EmanuelHerrador <emanuel.nicolas.herrador@unc.edu.ar>
2022-11-08 16:17:53 -0300

    Eliminadas lineas para no duplicar nodos en el tree

--- fat_fuse_ops.c
+++ fat_fuse_ops.c
@@ -27,8 +27,6 @@
 #define LOG_FILE_NAME LOG_FILE_BASENAME "." LOG_FILE_EXTENSION
 
 // ----------------------------- LOG FUNCTIONS -----------------------------
-static void fat_fuse_read_children(fat_tree_node dir_node);
-
 static void fat_fuse_log_init() {
     fat_volume vol = get_fat_volume();
     fat_tree_node file_node = NULL;
@@ -38,15 +36,6 @@ static void fat_fuse_log_init() {
     fat_tree_node dir_node = fat_tree_node_search(vol->file_tree, BB_DIRNAME);
     if (dir_node) { // ==> LOG File exists
         DEBUG("BB Directory exists ==> LOG File must to exists");
-
-        fat_fuse_read_children(dir_node); // Refresh BB Directory
-
-        file_node = fat_tree_node_search(vol->file_tree, BB_LOG_FILE);
-        if (file_node)
-            DEBUG("LOG File already exists");
-        else
-            DEBUG("ERROR --> LOG File doesn't exists");
-
         return;
     }
 
